0885f200fdc18998dc077a794e757f8a42aef0c7
Sharmila <107780645+sharmi2020@users.noreply.github.com>
2023-06-07 20:01:16 +0530

    Add files via upload
    counter using javascript

---
+++ index.js
@@ -0,0 +1,6 @@
+let button=document.querySelector("button");
+let text=document.querySelector("textarea");
+button.addEventListener("click",()=>{
+    let utterance=new SpeechSynthesisUtterance(text.value);
+    speechSynthesis.speak(utterance);
+});
